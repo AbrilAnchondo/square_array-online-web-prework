0071a59ca4f0f02fe3272de7bd90048dd65f83c8
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-28 15:15:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  #my_array = []
+ # array.each do |number|
+   # new_number = number**2
+  #  my_array.push(new_number)
+  #end
+  #return my_array
+  array.collect {|number| number**2}
+  end
+  
 end
